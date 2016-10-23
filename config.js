50790e05f2d9f80ba0264e6bcb4120e41d55fced
Simone Esposito <chaufnet@gmail.com>
2016-10-23 21:33:10 +0200

    Remove API key

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ const DB_SERVER_PATH = "postgres://postgres:postgres@localhost:5432/postgres"
 const SOCKET_SERVER_PORT = 5001
 const BASE_PATH = __dirname
 const APP_PATH = join(__dirname, "/app")
-const STEAM_API_KEY = "072B28626EB02FB62FCCDF0D5868AA7F"
+const STEAM_API_KEY = ""
 
 export { HTTP_SERVER_PATH, HTTP_SERVER_PORT, DB_SERVER_PATH, SOCKET_SERVER_PORT, BASE_PATH, APP_PATH, STEAM_API_KEY }
